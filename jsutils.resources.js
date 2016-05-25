4f91a031af93a0300d0ae93f425d819b74427332
Lalit Tanwar <lalit.tanwar07@gmail.com>
2016-05-26 01:11:50 +0530

    single request for resources

--- jsutils.resources.js
+++ jsutils.resources.js
@@ -4,12 +4,13 @@ define({
 }).as(function(RESOURCEUTIL, jq, fileUtil, cacheUtils, JSON, JSONPath, bootloader) {
 
     var cacheable = cacheUtils.instance("jsutils.resources");
+    var DFERRED = {};
     return {
         getJSON: function(fileNameJson) {
             var version = (bootloader && bootloader.config) ?
                 JSONPath("resource.version").load(bootloader.config(), bootloader.config().version) : 0;
             var fileName = fileNameJson,
-                fileNameKey = "_lang." + fileName;
+                fileNameKey = fileName;
             var langMap = cacheable.get(fileNameKey);
             if (langMap) {
                 langMap = JSON.parse(langMap);
@@ -17,11 +18,14 @@ define({
             if (langMap && langMap.version >= version) {
                 return jq.when(langMap);
             } else {
-                return fileUtil.getJSON(fileNameJson).then(function(resp) {
+                return (function(dff){
+                    DFERRED[fileNameJson] = dff;
+                    return DFERRED[fileNameJson];
+                })(DFERRED[fileNameJson] || fileUtil.getJSON(fileNameJson).then(function(resp) {
                     resp.version = version || resp.version;
                     cacheable.set(fileNameKey, JSON.stringify(resp));
                     return resp;
-                });
+                }));
             }
         }
     };
